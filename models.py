03e1e05a30075709632168107778e94342c6b783
TURKCELL\TTSKIR <mert.kir@turkcell.com.tr>
2020-05-10 18:14:58 +0300

    project completed

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -20,7 +19,20 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 32, 5) #(224-5)/1+1=220: 32 x 220 x 220 -> 32 x 110 x 110
+        self.conv2 = nn.Conv2d(32, 64, 3)#(110-3)/1+1=108: 64 x 108 x 108 -> 64 x 54 x 54
+        self.conv3 = nn.Conv2d(64, 128, 3)#(54-3)/1+1=52: 128 x 52 x 52 -> 128 x 26 x 26
+        self.conv4 = nn.Conv2d(128, 256, 3)#(26-3)/1+1=24: 256 x 24 x 24 -> 256 x 12 x 12
+        self.conv5 = nn.Conv2d(256, 512, 1)#(12-1)/1+1=12: 512 x 12 x 12 -> 512 x 6 x 6
+        
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.fc1 = nn.Linear(512*6*6, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 136)
+        
+        self.fc1_drop = nn.Dropout(p=0.2)
+        self.fc2_drop = nn.Dropout(p=0.3)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -32,6 +44,17 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.pool(F.relu(self.conv5(x)))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc2_drop(x)
+        x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
